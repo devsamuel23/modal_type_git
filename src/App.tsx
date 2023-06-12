0141432924c13bd186dad160e9e15f9b0e19e60e
Freitas <samuel.freitas@iob.com.br>
2023-06-12 09:50:51 -0300

    adicionando uma Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Modal from './components/Modal';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <Modal/>
         
       </header>
